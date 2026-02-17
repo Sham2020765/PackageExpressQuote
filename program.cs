c9a03c5338b94a2d67fb48fbae98306c1d6b47e5
Sham2020765 <Malak2000y@icloud.com>
2026-02-17 10:13:14 -0500

    Create program.cs

---
+++ program.cs
@@ -0,0 +1,36 @@
+using System.
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+        Console.WriteLine("Please enter the package weight:");
+        int weight = Convert.ToInt32(Console.ReadLine());
+
+        if (weight > 50)
+        {
+            Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
+            return;
+        }
+
+        Console.WriteLine("Please enter the package width:");
+        int width = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Please enter the package height:");
+        int height = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Please enter the package length:");
+        int length = Convert.ToInt32(Console.ReadLine());
+
+        if (width + height + length > 50)
+        {
+            Console.WriteLine("Package too big to be shipped via Package Express.");
+            return;
+        }
+
+        int dimensionProduct = width * height * length;
+        double quote = (dimensionProduct * weight) / 100.0;
+
+        Console.WriteLine("Your estimated total for shipping this package is: ${0:0.00}", quote);
+        Console.WriteLine("Thank you!");
+    }
+}
